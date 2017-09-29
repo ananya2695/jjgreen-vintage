5f0ceea7ec38a3ccde91a88f7a83d7ffc18de683
Sirintra Wannakheaw <sirintrawannakeaw@gmail.com>
2017-09-29 16:35:44 +0700

    update search.ts,search.html by mam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { EcommerceCoreModule, IonIconSearchbarComponent, IonListGridComponent ,IonFormProfileComponent } from "@ngcommerce/core";
+import { EcommerceCoreModule, IonIconSearchbarComponent, IonListGridComponent ,IonFormProfileComponent ,IonListProductComponent} from "@ngcommerce/core";
 import { HttpModule } from "@angular/http";
 
 import { MyApp } from './app.component';
@@ -14,6 +14,7 @@ import { CartPage } from '../pages/cart/cart';
 import { SearchPage } from '../pages/search/search';
 import { ProfilePage } from '../pages/profile/profile';
 import { LoginPage } from "../pages/login/login";
+import { Ionic2RatingModule } from 'ionic2-rating';
 
 @NgModule({
   declarations: [
@@ -27,11 +28,13 @@ import { LoginPage } from "../pages/login/login";
     IonIconSearchbarComponent,
     IonListGridComponent,
     IonFormProfileComponent,
+    IonListProductComponent,
     LoginPage
   ],
   imports: [
     BrowserModule,
     HttpModule,
+    Ionic2RatingModule,
     IonicModule.forRoot(MyApp),
     EcommerceCoreModule.forRoot('https://greenvintage-v2.herokuapp.com/api/')
   ],
--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { CorService, ProductListModel, ProductService } from "@ngcommerce/core";
+import { Http } from '@angular/http';
 /**
  * Generated class for the SearchPage page.
  *
@@ -14,12 +15,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'search.html',
 })
 export class SearchPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  product = {} as ProductListModel;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public productService: ProductService, public http: Http) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchPage');
+    this.getListProduct();
+  }
+  getListProduct(){
+    this.http.get('https://greenvintage-v2.herokuapp.com/api/products/')
+    .map(res => res.json())
+    .subscribe(data=>{
+      console.log(data);
+    })
   }
 
 }
