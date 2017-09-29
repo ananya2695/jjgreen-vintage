d874718ebe702c2e0629e975b653c396f74c37b5
tingtang35 <tingtang35@gmail.com>
2017-09-29 16:18:54 +0700

    add cart

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http,Response } from "@angular/http";
+import { CartModel } from "@ngcommerce/core";
 
 /**
  * Generated class for the CartPage page.
@@ -15,11 +17,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CartPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  cart = {} as CartModel;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CartPage');
   }
-
 }
