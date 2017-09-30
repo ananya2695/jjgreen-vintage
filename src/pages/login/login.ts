acd425a6b9cc92799a088bac55cac3efba7ad086
Sirintra Wannakheaw <sirintrawannakeaw@gmail.com>
2017-09-30 22:37:32 +0700

    login loading

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, LoadingController } from 'ionic-angular';
 import { CorService, UserModel, AuthenService } from "@ngcommerce/core";
 import { TabnavPage } from '../tabnav/tabnav';
 
@@ -18,21 +18,25 @@ import { TabnavPage } from '../tabnav/tabnav';
 })
 export class LoginPage {
   user = {} as UserModel;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public authenService: AuthenService, public viewCtrl: ViewController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public authenService: AuthenService, public viewCtrl: ViewController, public loading: LoadingController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
   login(user) {
+    let loading = this.loading.create();
+    loading.present();
     this.authenService.signIn(user).then((data) => {
       this.user = data; ///////////////////// บรรทัดนี้ตอนแรกยังไม่มี มาเขียนเพิ่มตอนที่จะไปโชว์ที่หน้าจอ ตามขั้นตอนด้านล่าง
       console.log(data);
-      window.localStorage.setItem('jjuser',JSON.stringify(data));
+      window.localStorage.setItem('jjuser', JSON.stringify(data));
+      loading.dismiss();
       // this.navCtrl.push(TabnavPage);
-      this.viewCtrl.dismiss(data);      
+      this.viewCtrl.dismiss(data);
     }, (error) => {
       console.error(error);
+      loading.dismiss();
     });
   }
 
