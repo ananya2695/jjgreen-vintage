0249a5b16120214e0459187a0ba4be33437af4a3
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-10-06 14:32:17 +0700

    update home segment

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, App, NavController, NavParams, ModalController } from 'ionic-angular';
-import { ProductListModel, ProductService } from "@ngcommerce/core";
+import { IonicPage, App, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { HomeService, HomeCategoryModel } from "@ngcommerce/core";
 import { LoginPage } from '../login/login';
-
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
 /**
  * Generated class for the HomePage page.
  *
@@ -19,50 +15,39 @@ import 'rxjs/add/operator/toPromise';
   templateUrl: 'home.html',
 })
 export class HomePage {
-  product = {} as ProductListModel;
-  homeData = {};
+
+  homeData = {} as HomeCategoryModel;
   pages: string;
 
-  constructor(public app: App, public navCtrl: NavController, public navParams: NavParams, public productService: ProductService, public http: Http, public modalCtrl: ModalController) {
+  constructor(
+    public app: App,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadingCtrl: LoadingController,
+    public homeService: HomeService
+  ) {
   }
 
   ionViewWillEnter() {
     console.log('ionViewDidLoad HomePage');
-    this.getListProduct();
     this.getHomeData();
   }
 
   getHomeData() {
     this.pages = '0';
-    this.http.get('https://greenvintage-v1.herokuapp.com/api/dataofcategories')
-      .toPromise()
-      .then((response) => {
-        this.homeData = response.json();
-        console.log(this.homeData);
-      })
-      .catch((handleError) => {
-        console.log(handleError);
-      });
+    let loading = this.loadingCtrl.create();
+    loading.present();
+    this.homeService.getHome().then((data) => {
+      this.homeData = data;
+      loading.dismiss();
+    }, (error) => {
+      console.log(error);
+      loading.dismiss();      
+    });
+
   }
 
   onSelectedPage(index) {
     this.pages = index;
   }
-
-  getListProduct() {
-    this.productService.getProductList().then((data) => {
-      this.product = data; ///////////////////// บรรทัดนี้ตอนแรกยังไม่มี มาเขียนเพิ่มตอนที่จะไปโชว์ที่หน้าจอ ตามขั้นตอนด้านล่าง
-      console.log(data);
-    }, (error) => {
-      let err = JSON.parse(error._body);
-      console.log(err);
-      if (err.message === 'Token is incorrect or has expired. Please login again') {
-        window.localStorage.removeItem('jjuser');
-        this.app.getRootNav().setRoot(LoginPage);
-      }
-      console.log(error);
-    });
-
-  }
-
 }
