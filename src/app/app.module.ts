c54c782c206ec57fac8198c8b421090911c632e4
mob35 <toonelicious@gmail.com>
2017-10-18 13:37:57 +0700

    root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -77,7 +77,7 @@ import { LoadingProvider } from '../providers/loading/loading';
     HttpModule,
     Ionic2RatingModule,
     IonicModule.forRoot(MyApp),
-    EcommerceCoreModule.forRoot('https://greenvintage-v2.herokuapp.com/api/')
+    EcommerceCoreModule.forRoot('https://greenvintage-v3.herokuapp.com/api/')
   ],
   bootstrap: [IonicApp],
   entryComponents: [
