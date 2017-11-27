980a2c2cdb732e8bea454b5b98a525429de04a85
Bu4y <amnes1a@windowslive.com>
2017-11-27 13:49:45 +0700

    update

--- src/app/app.contant.ts
+++ src/app/app.contant.ts
@@ -1,5 +1,5 @@
 export class Constants {
-    public static get URL(): string { return "https://greenvintage-v2.herokuapp.com/api/"; };
+    public static get URL(): string { return "https://greenvintage-v3.herokuapp.com/api/"; };
     public static get OmiseKey(): any {
         let key = {
             'publicKey': 'pkey_test_59owuo5m3qynavw8mac', //pkey_test_58zdlcecghhko63vy18
