3c65722c106c1c606e544b4e9cac2eaec58249ab
mob35 <toonelicious@gmail.com>
2017-12-06 18:27:17 +0700

    update

--- src/pages/form-address/form-address.ts
+++ src/pages/form-address/form-address.ts
@@ -1,10 +1,8 @@
+import { Dialogs } from '@ionic-native/dialogs';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { AddressModel } from "@ngcommerce/core";
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { CompleteServiceProvider } from '../../providers/complete-service/complete-service';
-import { Dialogs } from '@ionic-native/dialogs';
-
+import { Validators, FormGroup, FormControl, FormBuilder } from '@angular/forms';
 /**
  * Generated class for the FormAddressPage page.
  *
@@ -18,39 +16,89 @@ import { Dialogs } from '@ionic-native/dialogs';
   templateUrl: 'form-address.html',
 })
 export class FormAddressPage {
-  address = {} as AddressModel;
-  myForm: FormGroup;
+  // address = {} as AddressModel;
+  address: FormGroup;
   constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams, 
+    public navCtrl: NavController,
+    public navParams: NavParams,
     public viewCtrl: ViewController,
-    public completeServiceProvider: CompleteServiceProvider,
-    private dialogs:Dialogs
+    public formBuilder: FormBuilder,
+    private dialogs: Dialogs
   ) {
+    this.address = this.formBuilder.group({
+      firstname: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      subdistrict: new FormControl('', Validators.required),
+      district: new FormControl('', Validators.required),
+      province: new FormControl('', Validators.required),
+      postcode: new FormControl('', Validators.required),
+      tel: new FormControl('', Validators.required)
+    });
   }
 
   ionViewDidLoad() {
+
     console.log('ionViewDidLoad FormAddressPage');
   }
-
-  ngOnInit(): void {
-    this.myForm = new FormGroup({
-      country: new FormControl({}, [
-        Validators.required
-      ])
-    })
-  }
-
-  submit(): void {
-    this.dialogs.alert(JSON.stringify(this.myForm.value.country),'Form Address');
-    // let country = this.myForm.value.country;
-  }
-
   dismiss() {
     this.viewCtrl.dismiss();
   }
 
-  saveAddress() {
-    this.viewCtrl.dismiss(this.address);
+  validation_messages = {
+    'firstname': [
+      { type: 'required', message: 'กรุณากรอกชื่อ.' }
+    ],
+    'lastname': [
+      { type: 'required', message: 'กรุณากรอกนามสกุล.' }
+    ],
+    'address': [
+      { type: 'required', message: 'กรุณากรอกที่อยู่.' }
+    ],
+    'subdistrict': [
+      { type: 'required', message: 'กรุณากรอกตำบลหรือแขวง.' }
+    ],
+    'district': [
+      { type: 'required', message: 'กรุณากรอกอำเภอหรือเขต.' }
+    ],
+    'province': [
+      { type: 'required', message: 'กรุณากรอกจังหวัด.' }
+    ],
+    'postcode': [
+      { type: 'required', message: 'กรุณากรอกรหัสไปรษณีย์.' }
+    ],
+    'tel': [
+      { type: 'required', message: 'กรุณากรอกเบอร์โทร.' }
+    ]
+  };
+  saveAddress(values) {
+    if (values) {
+      if (!values.firstname) {
+        this.dialogs.alert('กรุณากรอกชื่อ');
+        return;
+      } else if (!values.lastname) {
+        this.dialogs.alert('กรุณากรอกนามสกุล');
+        return;
+      } else if (!values.address) {
+        this.dialogs.alert('กรุณากรอกที่อยู่');
+        return;
+      } else if (!values.subdistrict) {
+        this.dialogs.alert('กรุณากรอกตำบลหรือแขวง');
+        return;
+      } else if (!values.district) {
+        this.dialogs.alert('กรุณากรอกอำเภอหรือเขต');
+        return;
+      } else if (!values.province) {
+        this.dialogs.alert('กรุณากรอกจังหวัด');
+        return;
+      } else if (!values.postcode) {
+        this.dialogs.alert('กรุณากรอกรหัสไปรษณีย์');
+        return;
+      } else if (!values.tel) {
+        this.dialogs.alert('กรุณากรอกเบอร์โทร');
+        return;
+      }
+      this.viewCtrl.dismiss(values);
+    }
   }
 }
